c18e096988ee9369c4c8a187ef7fbdff1e480950
ruben marin <ruben@rosemontmedia.com>
2016-01-06 11:07:32 -0800

    code updated as constructor : new Paginathor();

--- assets/js/paginathor.js
+++ assets/js/paginathor.js
@@ -1,46 +1,89 @@
-var Paginathor = (function () {
-/* 
-	based on : http://www.strangerstudios.com/sandbox/pagination/diggstyle.php
+/*
+	https://carldanley.com/js-module-pattern/
 */
-	/* Private */
-	var _settings = { page : 1 , total : 0 , limit : 10 , adj : 1 , target : '#' , sep : '...'};
-	var _tmpl = {
-		/* example : function(MUST_BE_ARRAY) */
-		isSelected : function(arr){
-			return "<li class=\"selected\"><a>"+arr[0]+"</a></li>";
-		},
-		pageItem : function(arr){
-			return "<li><a class=\"submission-page\" data-submission-page=\""+arr[1]+"\" href=\""+arr[0]+arr[1]+"\">"+arr[1]+"</a></li>";
-		},
-		sep : function(arr){
-			return "<li><span class=\"elipses\">"+arr[0]+"</span></li>";
-		}
-	};
-	var _updateSettings = function(o){
-		for (var attrname in o) { _settings[attrname] = o[attrname]; }
-	};
-	var renderTmpl = function(tmplname , arr ){
-		var f = _tmpl[tmplname];
-		return f.call( this , arr );
-	};
-	/* PUBLIC */
-	return{
-		addTmpl: function( t ){
-			if(t){
-				for (var attrname in t) { _tmpl[attrname] = t[attrname]; }
-			}
-		},
-		getSettings:function(){
-			return _settings;
-		},
-		renderTmpl:function( tmplname , arr ){
-			return renderTmpl( tmplname , arr );
-		},
-		build: function( o ){
-			
-			_updateSettings( o );
+var Paginathor = ( function( window, undefined ) {
+	
+	/*****************************************/	
+	/** GLOBALS *****************************/
+	/***************************************/	
+	
+	// this object is used to store private variables and methods across multiple instantiations
+  
+	var privates = {};
 
-			var s        = _settings;
+	var renderTemplate = function( t , f , a ){ 
+
+		var TEMPLATES = t.templates;
+
+		TEMPLATES = (f) ? TEMPLATES[f] : TEMPLATES;
+		
+		return TEMPLATES.call( t , a );
+
+	};
+	function Paginathor(_S_) {
+	/*****************************************/	
+	/** PRIVATE *****************************/
+	/***************************************/	
+
+	/*****************************************/	
+	/** PUBLIC ******************************/
+	/***************************************/	
+	
+	/***** 
+	** DEFAULTS : Variables
+	*****/	
+	this._settings = { page : 1 , total : 0 , limit : 10 , adj : 1 , target : '#' , sep : '...' , elClass : 'pagi-item' , elData : ''};
+	/*
+		-- 	this._settings -> elClass : item-pagi
+			'elClass' sets the class="" and the data attribute for the clickable element
+			
+			based on above 'elClass' : class="item-pagi" , data-item-pagi=
+
+		--  this._settings -> elData : if you want to set a different data attribute
+	*/
+	this._S_ = _S_||{}; 
+
+	this.templates = {
+		/* example : function(MUST_BE_ARRAY) */
+		isSelected : function(a){
+			return "<li class=\"selected\"><a>"+a[0]+"</a></li>";
+		},
+		pageItem : function(a){
+
+			var s  = this.getSettings();
+			
+			return "<li><a class=\""+s.elClass+"\" data-"+ ( ( s.elData ) ? s.elData : s.elClass ) +"=\""+a[1]+"\" href=\""+a[0]+a[1]+"\">"+a[1]+"</a></li>";
+		},
+		sep : function(a){
+			return "<li><span class=\"ellipses\">"+a[0]+"</span></li>";
+		}
+	}  
+
+	/***** 
+	** FUNCTIONS 
+	*****/	    
+
+	this.getSettings = function(){
+		return this._settings;
+	};
+
+	this.updateSettings = function(o){
+		/*
+			rewrites this._settings , this._S_ is left alone
+		*/
+		for (var attrname in o) { this._settings[attrname] = o[attrname]; }	
+	};
+
+	this.build = function(c){
+
+		var s = ( c ) ? c : this._S_ ; /**/
+
+		this.updateSettings(s);
+		
+		s = this.getSettings();
+
+		
+			var self     = this;
 			var prev     = ( s.page - 1);
 			var next     = ( s.page + 1);
 			var lastpage = Math.ceil( s.total / s.limit );
@@ -48,15 +91,20 @@ var Paginathor = (function () {
 			var html = "";
 			
 			var firsttwo = "";
-				firsttwo += this.renderTmpl('pageItem' , [ s.target , '1']);
-				firsttwo += this.renderTmpl('pageItem' , [ s.target , '2']);
+				firsttwo += renderTemplate(self ,'pageItem' , [ s.target , '1']);
+				firsttwo += renderTemplate(self ,'pageItem' , [ s.target , '2']);
 
 			var lasttwo = "";
-				lasttwo += this.renderTmpl('pageItem' , [ s.target ,lpm1]);
-				lasttwo += this.renderTmpl('pageItem' , [ s.target ,lastpage]);
-			/* separator / ellipsis / placeholder */
-			var sep = this.renderTmpl('sep' , [s.sep]);
-
+				lasttwo += renderTemplate(self ,'pageItem' , [ s.target ,lpm1]);
+				lasttwo += renderTemplate(self ,'pageItem' , [ s.target ,lastpage]);
+			/** 
+			* separator / ellipsis / placeholder 
+			**/
+			var sep = renderTemplate(self ,'sep' , [s.sep]);
+			/* 
+				LOGIC START 
+				based on : http://www.strangerstudios.com/sandbox/pagination/diggstyle.php
+			*/
 			if(lastpage > 1){
 				/**
 				* not enough pages to bother breaking it up
@@ -64,10 +112,10 @@ var Paginathor = (function () {
 				if ( lastpage < 7 + ( s.adj * 2) ){		
 					for ( counter = 1; counter <= lastpage; counter++ ){
 						if( counter == s.page ){
-							html += this.renderTmpl('isSelected' , [ counter ]);
+							html += renderTemplate(self ,'isSelected' , [ counter ]);
 						}
 						else{
-							html += this.renderTmpl('pageItem' , [ counter ]);
+							html += renderTemplate(self ,'pageItem' , [ counter ]);
 						}				
 					}
 				}
@@ -80,48 +128,59 @@ var Paginathor = (function () {
 						for ( counter = 1; counter < 4 + ( s.adj * 2); counter++)
 						{
 							if ( counter == s.page){
-								html += this.renderTmpl('isSelected' , [ counter ]);
+								html += renderTemplate(self ,'isSelected' , [ counter ]);
 							}
 							else{
-								html += this.renderTmpl('pageItem' , [ s.target , counter ]);
+								html += renderTemplate(self ,'pageItem' , [ s.target , counter ]);
 							}				
 						}
 						html += sep;
 						html += lasttwo;
 					}
-					/*in middle; hide some front and some back */
+					/*
+						in middle; hide some front and some back 
+					*/
 					else if(lastpage - (s.adj * 2) > s.page && s.page > (s.adj * 2)){
 						html += firsttwo;
 						html += sep;
 						for (counter = s.page - s.adj; counter <= s.page + s.adj; counter++)
 						{
 							if (counter == s.page){
-								html += this.renderTmpl('isSelected' , [ counter ]);
+								html += renderTemplate(self ,'isSelected' , [ counter ]);
 							}
 							else{
-								html += this.renderTmpl('pageItem' , [ s.target , counter ]);
+								html += renderTemplate(self ,'pageItem' , [ s.target , counter ]);
 							}			
 						}
 						html += sep;
 						html += lasttwo;	
 					}
-					/* close to end; only hide early pages */
+					/* 
+						close to end; only hide early pages 
+					*/
 					else{
 						html += firsttwo;
 						html += sep;
 						for (counter = lastpage - ( 1 + (s.adj * 3)); counter <= lastpage; counter++ )
 						{
 							if (counter == s.page){
-								html += this.renderTmpl('isSelected' , [ counter ]);
+								html += renderTemplate(self ,'isSelected' , [ counter ]);
 							}
 							else{
-								html += this.renderTmpl('pageItem' , [ s.target , counter ]);				
+								html += renderTemplate(self ,'pageItem' , [ s.target , counter ]);				
 							}
 						}
 					}				
 				}
 			}
+			/* 
+				LOGIC END 
+			*/
 			return html;
-		}
+
+		
 	};
-})();
+
+	}
+	return Paginathor;
+} )( window );
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,18 +6,22 @@ $(function() {
 		//<div id="pagination-wrapper"><ul></ul></div> 
 		pagiWrapper.innerHTML = paginatehtml;
 	*/
-	
-	var paginatehtml = Paginathor.build({ page : 1 , total : 214 , limit : 10 , adj : 1 , target : '#'});
+
+	var myPagi = new Paginathor({ page : 1 , total : 214 , limit : 10 , adj : 1 , target : '#' , elClass : 'submission-page' });
+
+	var pagiBuild = myPagi.build();
+
 	var pagiWrapper = $('#pagiWrapper ul');
-	pagiWrapper.html(paginatehtml);
+	pagiWrapper.html(pagiBuild);
 	
 	function paginateClicker(){
 		$('.submission-page').on('click', function(e){
 			e.preventDefault();
 			var t = $(this);
 			var p = t.data('submissionPage');
-			html = Paginathor.build({ page : p });
+			html = myPagi.build({ page : p });
 			pagiWrapper.html(html);
+			console.log(myPagi);
 			paginateClicker();
 		});	
 	}
